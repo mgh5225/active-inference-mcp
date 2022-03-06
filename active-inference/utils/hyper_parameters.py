54195bce9340c05bc7b8c8701c56636b959d9c7c
Mohammad Ghazanfari <mgh.5225@gmail.com>
2022-03-06 12:47:25 +0330

    hyper parameters added

---
+++ active-inference/utils/hyper_parameters.py
@@ -0,0 +1,16 @@
+d_s = 10
+d_o = 1
+d_a = 1
+
+adam_alpha = 0.001
+adam_beta_1 = 0.9
+adam_beta_2 = 0.999
+adam_epsilon = 10e-8
+
+n_F = 1
+n_pop = 10e4
+
+steps = 10000
+
+sig_min = 1e-6
+sig_init = -3
