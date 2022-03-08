a248b451a6be9b3b73aaebd8d13af3199edc5b1d
Mohammad Ghazanfari <mgh.5225@gmail.com>
2022-03-08 16:30:18 +0330

    unity env updated

--- unity/Assets/Scripts/PopulationController.cs
+++ unity/Assets/Scripts/PopulationController.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.MLAgents;
+using TMPro;
 
 public class PopulationController : MonoBehaviour
 {
     [SerializeField] Transform startPosition;
     [SerializeField] Transform carAgent;
     [SerializeField] Transform target;
+    [SerializeField] TMP_Text agentsText;
     int n_pop;
     EnvironmentParameters env;
     void Start()
@@ -18,6 +20,9 @@ public class PopulationController : MonoBehaviour
         Academy.Instance.OnEnvironmentReset += () =>
         {
             n_pop = (int)env.GetWithDefault("n_pop", 1f);
+
+            agentsText.text = $"Agents: {n_pop.ToString()}";
+
             for (int i = 1; i < n_pop; i++)
             {
                 var agent = Instantiate(carAgent, startPosition.position, Quaternion.identity);
